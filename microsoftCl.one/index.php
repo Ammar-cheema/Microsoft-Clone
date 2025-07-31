a64ef0ef0d8fb321b724248bbf4d125a5258d5e0
Ammar Qaiser <157932845+Ammar-cheema@users.noreply.github.com>
2025-07-31 15:03:12 +0500

    Add files via upload
    initial commmit

---
+++ microsoftCl.one/index.php
@@ -0,0 +1,9 @@
+<?php 
+
+
+$connection = mysqli_connect($username , $database , $servername , $password) or die("cannot connect to the database ".mysqli_connect_error())
+
+$connection = mysqli_connect(all variablles name)
+or die("not connected ".mysqli_connect_error)
+
+?>
